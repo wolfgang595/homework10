769ad2b6e2e072d7a3e2c89825a2dcf830307b11
Nathan Serna <nath2534@outlook.com>
2024-11-03 07:28:33 -0600

    hello world fetch is working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
-
+import route from "./routes/employeeRoute.js"
 
 
 
@@ -26,7 +26,7 @@ mongoose.connect(MONGOURL).then( ()=> {
 
 
 
-
+app.use("/api/employees", route)
 
 
 
