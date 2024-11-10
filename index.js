25bb8dd3dce8b4dd7f939959fcec81e415b12780
Nathan Serna <nath2534@outlook.com>
2024-11-10 15:29:48 -0600

    installed cors

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import route from "./routes/employeeRoute.js"
-
+import cors from "cors"
 
 
 
 
 const app = express()
 app.use(bodyParser.json())
-
+app.use(cors())
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
